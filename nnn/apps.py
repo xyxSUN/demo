a8211b9bbca50c7dde72ef3336010168b15ce85a
xyxsun <xyxsun@foxmail.com>
2018-07-27 20:00:13 +0800

    first blood

---
+++ nnn/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NnnConfig(AppConfig):
+    name = 'nnn'
---
+++ qqq/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QqqConfig(AppConfig):
+    name = 'qqq'
